c6ddc0e8a112932bef30b9291221f7e1638658b4
nikit <snikit086@gmail.com>
2025-03-23 01:23:33 +0530

    add frontend and rtc logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import  WebSocket, { WebSocketServer } from "ws";
 const wss = new WebSocketServer({port:8080});
 
 let senderSocket:null|WebSocket = null;
-let recieverSocket:null|WebSocket = null;
+let receiverSocket:null|WebSocket = null;
 
 
 wss.on('connection', function connection(ws) {
@@ -14,23 +14,23 @@ wss.on('connection', function connection(ws) {
         const message = JSON.parse(data);
         if(message.type === 'sender') {
             senderSocket = ws;
-        }else if (message.type === 'reciever') {
-            recieverSocket = ws;
+        }else if (message.type === 'receiver') {
+            receiverSocket = ws;
         } else if (message.type === 'createOffer') {
             if(ws!==senderSocket) {
                 return;
             }
-            recieverSocket?.send(JSON.stringify({type:"createOffer", sdp:message.sdp}));
+            receiverSocket?.send(JSON.stringify({type:"createOffer", sdp:message.sdp}));
         } else if (message.type === 'createAnswer') {
-            if(ws!==recieverSocket) {
+            if(ws!==receiverSocket) {
                 return;
             }
             senderSocket?.send(JSON.stringify({type:'createAnswer' , sdp:message.sdp}));
         } else if (message.type === 'iceCandidate') {
-            if(ws === recieverSocket) {
+            if(ws === receiverSocket) {
                 senderSocket?.send(JSON.stringify({type:'iceCandidate' , candidate:message.candidate}))
             }else if (ws === senderSocket) {
-                recieverSocket?.send(JSON.stringify({type : 'iceCandidate' , candidate : message.candidate}))
+                receiverSocket?.send(JSON.stringify({type : 'iceCandidate' , candidate : message.candidate}))
             }
         }
     });
